fdd0fd0ed22e7c1bba975898a95bf84fd0b3b482
Charis Kyriakou <charisk@github.com>
2022-06-08 11:11:13 +0200

    Beautify alerts

--- blocks/sarif-file-block/index.tsx
+++ blocks/sarif-file-block/index.tsx
@@ -5,18 +5,50 @@ import "./index.css";
 export default function (props: FileBlockProps) {
   const { context, content, metadata } = props;
 
-  const alerts = [];
+  // Extract alert information from JSON.
+  const alerts: Alert[] = [];
   for (const run of JSON.parse(content).runs) {
     for (const result of run.results) {
-      alerts.push(result.message.text);
+      const message = result.message.text;
+      for (const location of result.locations) {
+        const codeSnippet = location.physicalLocation.contextRegion.snippet.text;
+        alerts.push({
+          message,
+          codeSnippet
+        });
+      }
     }
   }
 
+  // Render alerts
   return (
     <Box p={4}>
       {
-        alerts.map(a => <Box p={2}><pre>{a}</pre></Box>)
+        alerts.map(a => 
+          <div>
+            <AlertView {...a} />
+            <p></p>
+          </div>)
       }
+    </Box>
+  );
+}
+
+
+interface Alert {
+  message: string;
+  codeSnippet: string;
+}
+
+const AlertView = ({ message, codeSnippet }: Alert) => {
+  return (
+    <Box>
+      <Box className="message">{message}</Box>
+      <Box paddingLeft={3}>
+        <pre className="code-snippet"> 
+          {codeSnippet.trim()}
+        </pre>
+      </Box>
     </Box>
   );
 }
